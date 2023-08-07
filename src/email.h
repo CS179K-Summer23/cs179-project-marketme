36bb25c55bae19a06541822cb279cdb7ed4d6fe5
Henry Pham <108387314+hpham096@users.noreply.github.com>
2023-08-07 16:19:29 -0700

    fixed syntax errors

--- src/email.h
+++ src/email.h
@@ -6,16 +6,16 @@
 
 using namespace std;
 
-CONST string SENDER = "hpham096@ucr.edu";
-CONST string RECIPIENT = "phry.ham24@gmail.com";
-CONST string SUBJECT = "TEST";
-CONST string body = "testing...........";
+const string SENDER = "hpham096@ucr.edu";
+const string RECIPIENT = "phry.ham24@gmail.com";
+const string SUBJECT = "TEST";
+const string body = "testing...........";
 
-CONST string CREDENTIALS_PATH = "../data/credentials.json";
-CONST string ACCESS_TOKEN = "ya29.a0AfB_byD1nu0rfNHJ1vy_6SkqXobig80OWXDlkluYcc0F6vryzflAfQ2qqeZs3mf2ON_d-XqF7iLKR_SO5IbjKsY2TG_O0PVupRvrfDP_3STin-zeGkAPZGGvEhMghOSfYmtlEPfzNc8aJPAerYmsHq9y18ZLanUaCgYKAU0SARESFQHsvYlszZ6C16Y6p7i1seOEIzEl3A0166";
+const string CREDENTIALS_PATH = "../data/credentials.json";
+const string ACCESS_TOKEN = "ya29.a0AfB_byD1nu0rfNHJ1vy_6SkqXobig80OWXDlkluYcc0F6vryzflAfQ2qqeZs3mf2ON_d-XqF7iLKR_SO5IbjKsY2TG_O0PVupRvrfDP_3STin-zeGkAPZGGvEhMghOSfYmtlEPfzNc8aJPAerYmsHq9y18ZLanUaCgYKAU0SARESFQHsvYlszZ6C16Y6p7i1seOEIzEl3A0166";
                              // need to change this on playgrund every 3600 seconds
 
-CONST string COMMAND = "curl -X POST -H \"Authorization: Bearer " + ACCESS_TOKEN +
+const string COMMAND = "curl -X POST -H \"Authorization: Bearer " + ACCESS_TOKEN +
                         "\" -H \"Content-Type: application/json\" -d '" +
                         "{ \"raw\": \"RAW_EMAI_CONTENT_BASE64_ENCDED\" }' https://gmail.googleapis.com/gmail/v1/users/me/messages/send";
 
@@ -30,4 +30,4 @@ void sendEmail(){
     }
 }
 
-#endif EMAIL_H
+#endif
