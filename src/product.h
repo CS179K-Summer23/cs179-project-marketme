03cf636a090d58d9b8142653cb97cc0912229768
iwu021 <81645935+iwu021@users.noreply.github.com>
2023-08-09 05:39:09 -0700

    Added Check Out System with Promotion JSON

--- src/product.h
+++ src/product.h
@@ -2,64 +2,106 @@
 #define PRODUCT_H
 
 #include <iostream>
+
 #include <fstream>
+
 #include <unordered_set>
+
 #include "../libraries/json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
 class Product {
-public: 
-    string _id;
-    string _name;
-    string _description;
-    double _price;
-    int _quantity;
-    string _category;
-    string _sku;
-    string _barcode;
-    string _expiration_date;  // Expiration date in "YYYY-MM-DD" format
+  public: string _id;
+  string _name;
+  string _description;
+  double _price;
+  int _quantity;
+  string _category;
+  string _sku;
+  string _barcode;
+  string _expiration_date; // Expiration date in "YYYY-MM-DD" format
 
-    Product(const string& id, const string& name, const string& description, double price, 
-            int quantity, const string& category, const string& sku, const string& barcode, 
-            const string& expiration_date) {
-        _id = id;
-        _name = name;
-        _description = description;
-        _price = price;
-        _quantity = quantity;
-        _category = category;
-        _sku = sku;
-        _barcode = barcode;
-        _expiration_date = expiration_date;
-    }
+  Product(const string & id,
+    const string & name,
+      const string & description, double price,
+        int quantity,
+        const string & category,
+          const string & sku,
+            const string & barcode,
+              const string & expiration_date) {
+    _id = id;
+    _name = name;
+    _description = description;
+    _price = price;
+    _quantity = quantity;
+    _category = category;
+    _sku = sku;
+    _barcode = barcode;
+    _expiration_date = expiration_date;
+  }
 
-    bool validate(){
-        // You may add more validations based on your business rules
-        if(_id.empty() || _name.empty() || _price < 0 || _quantity < 0 || 
-           _category.empty() || _sku.empty() || _barcode.empty() || _expiration_date.empty()) {
-            return false;
-        }
-        if(_description.empty()){
-            _description.assign("NA");
-        }
-        return true;
-    }
+  double getPrice() const {
+    return _price;
+  }
 
-    json toJson() const {
-        return {
-            {"id", _id},
-            {"name", _name},
-            {"description", _description},
-            {"price", _price},
-            {"quantity", _quantity},
-            {"category", _category},
-            {"sku", _sku},
-            {"barcode", _barcode},
-            {"expiration_date", _expiration_date}
-        };
+  string getName() const {
+    return _name;
+  }
+
+  bool validate() {
+    // You may add more validations based on your business rules
+    if (_id.empty() || _name.empty() || _price < 0 || _quantity < 0 ||
+      _category.empty() || _sku.empty() || _barcode.empty() || _expiration_date.empty()) {
+      return false;
     }
+    if (_description.empty()) {
+      _description.assign("NA");
+    }
+    return true;
+  }
+
+  json toJson() const {
+    return {
+      {
+        "id",
+        _id
+      },
+      {
+        "name",
+        _name
+      },
+      {
+        "description",
+        _description
+      },
+      {
+        "price",
+        _price
+      },
+      {
+        "quantity",
+        _quantity
+      },
+      {
+        "category",
+        _category
+      },
+      {
+        "sku",
+        _sku
+      },
+      {
+        "barcode",
+        _barcode
+      },
+      {
+        "expiration_date",
+        _expiration_date
+      }
+    };
+  }
 };
 
 #endif
